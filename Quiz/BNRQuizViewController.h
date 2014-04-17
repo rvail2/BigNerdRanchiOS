18406d08647244be29263dd0a0500e945c52da6f
Remmie Vail <remmievail@Remmies-Mac-mini.local>
2014-04-16 21:40:02 -0500

    Chapter 1

---
+++ Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Remmie Vail on 4/4/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
---
+++ Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Remmie Vail on 4/4/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
